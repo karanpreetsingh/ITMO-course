6fe53ae6d32de05937a7d125938758f3d12d8427
Aaditya Sharma <aadityaja@gmail.com>
2016-11-03 00:20:15 +0530

    Update w2q1.java
    Added StringBuilder to speed up things

--- w2q1.java
+++ w2q1.java
@@ -16,19 +16,21 @@ public class Main  {
 		int n = Integer.parseInt(str);
 		Stack stack = new Stack();
 		FileWriter writer = new FileWriter("stack.out");
+		StringBuilder sb = new StringBuilder("");
 		while(n > 0){
 			str = br.readLine();
 			//What is faster : str.split(" ") or using this substring() method?
 			if(str.length() > 1)
 				stack.push(str.substring(2));
 			else{
+				sb.append(""+stack.pop()+"\n");
 				
-				writer.write(""+stack.pop()+"\n");
 			}
 
 			n--;
 			
-		}		
+		}
+		writer.write(""+sb);		
 		br.close();
 		writer.close();	
 			
